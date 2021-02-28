e621d83a6626600d579357327dd984c4fcb720ca
Kulvir Singh <kulvir.singh2019@vitstudent.ac.in>
2021-02-28 19:47:53 +0530

    threads

---
+++ threads.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+int i = 6;
+
+void* Thread(void* p){
+
+  printf("Value recevied as argument in starting routine: ");
+  printf("%i\n", * (int*)p);
+  return &i;
+}
+
+int main(void){
+
+  pthread_t id;
+  int j = 1;
+  pthread_create(&id, NULL, Thread, &j);
+    
+  int* ptr;
+
+  pthread_join(id, (void**)&ptr);
+  printf("Value recevied by parent from child: ");
+  printf("%i\n", *ptr);
+}
