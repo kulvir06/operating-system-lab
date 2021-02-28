c36d843769144bdef731b6b89390153c6fe559a6
Kulvir Singh <kulvir.singh2019@vitstudent.ac.in>
2021-02-28 20:03:09 +0530

    d

---
+++ d.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+int factorial = 1;
+
+void* Thread( void* p ) {
+    int x = * (int*)p;
+    for( int i=1; i<=x; i++ ) {
+        if( x%i==0 ) { 
+            factorial *= i;
+        }
+    }
+    return &factorial;    
+}
+
+int main() {    
+    int num;
+    printf("enter a number\n");
+    scanf("%d",&num);
+
+    pthread_t id;
+
+    pthread_create(&id, NULL, Thread, &num);
+    int* ptr;
+    pthread_join(id, (void**)&ptr);
+    printf("Factorial = %d",*ptr);
+    return 0;
+}
--- threads.c
+++ threads.c
@@ -4,14 +4,13 @@
 
 int i = 6;
 
-void* Thread(void* p){
-
+void* Thread( void* p ) {
   printf("Value recevied as argument in starting routine: ");
-  printf("%i\n", * (int*)p);
+  printf("%d\n", * (int*)p);
   return &i;
 }
 
-int main(void){
+int main() {
 
   pthread_t id;
   int j = 1;
@@ -21,5 +20,5 @@ int main(void){
 
   pthread_join(id, (void**)&ptr);
   printf("Value recevied by parent from child: ");
-  printf("%i\n", *ptr);
+  printf("%d\n", *ptr);
 }
