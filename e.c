cc1a9284eb465e509dce708b06a91bcdf6c9080e
Kulvir Singh <kulvir.singh2019@vitstudent.ac.in>
2021-03-01 01:18:56 +0530

    question e

--- e.c
+++ e.c
@@ -1,20 +1,41 @@
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
 
-void* Thread( void* p[] ) {
-    char new_str =  *(char*)p;
-    printf("%s",new_str);
-    // return &new_str;  
-}
+#include<stdio.h> 
+#include<sys/wait.h> 
+#include<unistd.h> 
+#include<sys/types.h>
 
+void Client();
+void Server();
+
+int ch[26];
 int main() {
-
-  pthread_t id;
-  char str[] = "abcdefghijklmnopqrstuvwxyz";
-  pthread_create(&id, NULL, Thread, &str);
-    
-//   char* ptr[];
-//   pthread_join(id, (void**)&ptr);
-//   printf("%s\n", *ptr);
-} 
+    printf("\n\n****OUTPUT****\n\n"); 
+    pid_t pid;
+    pid = fork();
+    if( pid==0 ) { Client(); }
+    else {
+        printf("Data stored by server in memory ------>\n");
+        Server();
+        printf("Client accessing the same memory location\n");
+        wait(NULL);
+    }
+}
+void Server() {
+  int k = 97;
+  for(int i=0;i<26;i++) {
+    ch[i] = k;
+    k++;
+    printf("%c",ch[i]);    
+  }  
+  printf("\n");  
+}
+void Client() {
+  printf("Data modified by client------>\n");
+  int k=65;
+    for(int i=0;i<26;i++) {
+    ch[i] = k;
+    k++;
+    printf("%c",ch[i]);    
+  }  
+  printf("\n");     
+}
